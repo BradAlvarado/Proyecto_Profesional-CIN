0def0043d746ab5a6592afcac501577f52343406
Brad Alvarado <brad.ralvarado@gmail.com>
2024-03-13 14:12:37 -0600

    Roles

--- Models/Roles.cs
+++ Models/Roles.cs
@@ -5,6 +5,8 @@ namespace Sistema_CIN.Models
 {
     public partial class Roles
     {
+        internal object nombre_rol;
+
         public int id_rol { get; set; }
         public string? nombre_Rol { get; set; }
 
